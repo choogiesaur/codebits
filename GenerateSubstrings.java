6453153677a453df635d4b469a3d2d04eb1d013b
Firas S <choogiesaur@users.noreply.github.com>
2018-08-27 20:31:23 -0400

    Update GenerateSubstrings.java

--- GenerateSubstrings.java
+++ GenerateSubstrings.java
@@ -10,9 +10,7 @@ public class GenerateSubstrings{
         //            01234
         
         for(int i = 0; i < tst.length(); i++){ // every poss substring length
-            //System.out.println("Substring length: " + (tst.length()-i));
             for(int j = 0; j < tst.length()-i; j++){
-                //System.out.println("j:" + j);
                 System.out.println(tst.substring(j,tst.length()-i));                
             }
         }
