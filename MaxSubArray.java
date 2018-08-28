ebd34340e2d47add6dc126c26e31a1fe0d90e55e
Firas S <choogiesaur@users.noreply.github.com>
2018-08-28 18:59:05 -0400

    Create MaxSubArray.java

---
+++ MaxSubArray.java
@@ -0,0 +1,31 @@
+// given integer array, recursively find contiguous subarray with max sum
+class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int pos = 0;
+        int num = nums.length;
+        int maxSum = Integer.MIN_VALUE;
+        //int[] newArray = Arrays.copyOfRange(oldArray, startIndex, endIndex);
+        
+        if(num == 1){
+            return nums[pos];
+        }
+        else if(num == 0){
+            return 0;
+        }else{
+            int sum = sumArray(nums, pos, num);
+            //System.out.println("Sum: " + sum);
+            //System.out.println("pos: " + pos + " num: " + num);
+            int numLess = Math.max(sum, maxSubArray(Arrays.copyOfRange(nums, pos, pos+num-1))); //recursive call with shorter subarray
+            int posMore = Math.max(sum, maxSubArray(Arrays.copyOfRange(nums, pos+1, pos+num))); // recursive call with next starting pos
+            return Math.max(numLess,posMore);
+        }
+    }
+    
+    public int sumArray(int arr[], int start, int num){
+        int sum = 0;
+        for (int i = start; i < num; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
