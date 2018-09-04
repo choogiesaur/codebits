f0f99e0ab5d48c5176993eea434927ad2de13a67
Firas S <choogiesaur@users.noreply.github.com>
2018-09-04 19:45:04 -0400

    Update MultipleStringKeys.java

--- MultipleStringKeys.java
+++ MultipleStringKeys.java
@@ -7,18 +7,18 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class MultipleStringKeys {
-    
+
     // helper class; a (String, String) key pair that can be hashed
     public static class StringKey {
-        
+
         public final String s1;
         public final String s2;
-        
-        public StringKey(String s1, String s2){
-            this.s1=s1;
-            this.s2=s2;
+
+        public StringKey(String s1, String s2) {
+            this.s1 = s1;
+            this.s2 = s2;
         }
-        
+
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
@@ -29,7 +29,7 @@ public class MultipleStringKeys {
             //return (this.s1.equals(key.s1) && this.s2.equals(key.s2)) || (this.s1.equals(key.s2) && this.s2.equals(key.s1));
             return (this.s1.equals(key.s1) && this.s2.equals(key.s2));
         }
-        
+
         @Override
         public int hashCode() {
             final int prime = 31;
@@ -41,32 +41,32 @@ public class MultipleStringKeys {
     }
 
     public static void main(String[] args) throws IOException {
-        Hashtable<StringKey, Integer> table = new Hashtable<StringKey, Integer>();
-        
-        StringKey x = new StringKey("apple","banana");
+        Hashtable < StringKey, Integer > table = new Hashtable < StringKey, Integer > ();
+
+        StringKey x = new StringKey("apple", "banana");
         table.put(x, 1);
-        
-        StringKey y = new StringKey("apple","grape");
+
+        StringKey y = new StringKey("apple", "grape");
         table.put(y, 1337);
-        
-        StringKey z = new StringKey("apple","pear");
+
+        StringKey z = new StringKey("apple", "pear");
         table.put(z, 999);
-      	
+
         System.out.println(table.containsKey(x));
-      	System.out.println(table.get(x));
-      	System.out.println(table.get(y));
-      	System.out.println(table.get(z));
-    	System.out.println();
-      
-      	table.put(z, 91919); 
-      	System.out.println(table.get(z));
-      	System.out.println();	
-      
-      	StringKey x2 = new StringKey("banana","apple");
-      	System.out.println(x.equals(x2));
-      	table.put(x2, 333);
-      	System.out.println(table.get(x2));
-      	System.out.println(table.get(x));
-      
+        System.out.println(table.get(x));
+        System.out.println(table.get(y));
+        System.out.println(table.get(z));
+        System.out.println();
+
+        table.put(z, 91919);
+        System.out.println(table.get(z));
+        System.out.println();
+
+        StringKey x2 = new StringKey("banana", "apple");
+        System.out.println(x.equals(x2));
+        table.put(x2, 333);
+        System.out.println(table.get(x2));
+        System.out.println(table.get(x));
+
     }
 }
