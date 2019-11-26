11c9e47c873ed3fa585b29d3d2d03beda9e7839a
Firas S <choogiesaur@users.noreply.github.com>
2019-11-25 23:48:06 -0500

    Update png2jpg.py

--- png2jpg.py
+++ png2jpg.py
@@ -1,6 +1,7 @@
 from PIL import Image  # Python Image Library - Image Processing\
 import glob
 
+# globs through directory for .png files, converts them using PIL (or Pillow) to .jpg
 for file in glob.glob("*.png"):
 	print file
 	im = Image.open(file)
