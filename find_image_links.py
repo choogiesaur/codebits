31864d8917167f9f0ea89ccca1863bbfb103b418
Firas S <choogiesaur@users.noreply.github.com>
2019-12-18 23:36:01 -0500

    Update find_image_links.py

--- find_image_links.py
+++ find_image_links.py
@@ -1,5 +1,6 @@
 import urllib2, re
-source = open('source.txt').read()
 
+# Ran this on source for page on bulbapedia that had pokemon GBC sprites
+source = open('source.txt').read()
 for link in re.findall('archives.bulbagarden.net(.*?).png', source):
 	print('archives.bulbagarden.net'+link+'.png')
