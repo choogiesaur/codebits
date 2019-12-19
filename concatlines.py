98c4868a58755df3ca5a2f1be4e252118ba8586d
Firas S <choogiesaur@users.noreply.github.com>
2019-12-18 22:21:35 -0500

    Update concatlines.py

--- concatlines.py
+++ concatlines.py
@@ -1,5 +1,13 @@
-# Iterate over lines of two files at once and concat individual lines
-###
+# Iterate over lines of two files at once and concatenate individual lines
+# Example:
+# file 1:   file 2:
+# a         lol
+# b         blah
+# c         mango
+# outfile:
+# a lol
+# b blah
+# c mango
 
 with open('file1.txt') as bf1:
     with open('file2.txt') as bf2:
