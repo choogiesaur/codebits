cc26c60acd8e18a91f15f7b588efa9adff69608f
firas <choogiesaur@users.noreply.github.com>
2021-02-23 15:43:12 -0500

    Undo subdirectories

--- python-codebits/find_image_links.py
+++
@@ -1,6 +0,0 @@
-import urllib2, re
-
-# Ran this on source for page on bulbapedia that had pokemon GBC sprites
-source = open('source.txt').read()
-for link in re.findall('archives.bulbagarden.net(.*?).png', source):
-	print('archives.bulbagarden.net'+link+'.png')
