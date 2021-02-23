bf01bd4b67bb157d4aea1c90013c315617169da7
Firas S <choogiesaur@users.noreply.github.com>
2021-02-23 14:27:33 -0500

    Multiple context managers in one line

--- concat_lines.py
+++ concat_lines.py
@@ -9,7 +9,9 @@
 # b blah
 # c mango
 
-with open('file1.txt') as bf1:
-    with open('file2.txt') as bf2:
-        for line1, line2 in zip(bf1, bf2):
-            print(line1.strip() + ' ' + line2.strip())
+file_1 = 'file1.txt'
+file_2 = 'file2.txt'
+
+with open(file_1) as bf1, open(file_2) as bf2:
+    for line1, line2 in zip(bf1, bf2):
+        print(line1.strip() + ' ' + line2.strip())
