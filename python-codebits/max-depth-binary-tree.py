9a1ccf99652eb23912b3a0bc64ea7194e7c44c6a
Firas S <choogiesaur@users.noreply.github.com>
2021-03-07 16:36:04 -0500

    Create max-depth-binary-tree.py

---
+++ python-codebits/max-depth-binary-tree.py
@@ -0,0 +1,12 @@
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+def maxDepth(self, root: TreeNode) -> int:
+  
+    # Visit root; if none, no extra depth discovered
+    if root == None:
+        return 0
+    # If not none; max depth is 1 extra level discovered + max of left and right subtrees
+    else:
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return 1 + max(left, right)
