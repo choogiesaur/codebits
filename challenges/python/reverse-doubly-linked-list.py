e5013fcd1f7bfc87a1e32784c61ce2d3f59502ec
Firas S <choogiesaur@users.noreply.github.com>
2022-02-17 16:55:00 -0500

    Create reverse-doubly-linked-list.py

---
+++ challenges/python/reverse-doubly-linked-list.py
@@ -0,0 +1,74 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+# The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#Write your code here
+# [] = [] = [] = [] = []
+def reverse(llist):
+    # keep track of current node and one to the right of it
+    curr = llist
+    prev = None
+    
+    while curr:
+        temp = curr
+        temp2 = curr.next
+        
+        temp.next = prev
+        temp.prev = temp2
+        
+        prev = temp
+        curr = temp2
+    return prev
+
+if __name__ == '__main__':
