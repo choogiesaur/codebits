b4e656cfb963e8f1cd37e8d7c4471b1c08356162
Firas S <choogiesaur@users.noreply.github.com>
2022-02-17 12:46:05 -0500

    Solved for all cases

--- challenges/python/sherlock-valid-string.py
+++ challenges/python/sherlock-valid-string.py
@@ -1,8 +1,6 @@
-# https://www.hackerrank.com/challenges/sherlock-and-valid-string
-# Solution is close, but need to solve the case where:
-# aabbc
-# 2 unique frequencies but the majority are frequency 2, need to recognize that you can delete the lone c
 #!/bin/python3
+# https://www.hackerrank.com/challenges/sherlock-and-valid-string
+# Solved for all cases!
 
 import math
 import os
@@ -18,6 +16,8 @@ from collections import defaultdict
 # The function accepts STRING s as parameter.
 #
 
+# aaaaabc
+# aaaabbcc
 def isValid(s):
     # Write your code here
     found_letters = set()
@@ -26,7 +26,8 @@ def isValid(s):
     for letter in s:
         found_letters.add(letter)
         letter_frequencies[letter] += 1
-    
+    print("found letters", found_letters)
+        
     # num_unique_frequencies = 0
     unique_frequencies = set()
     frequency_counts = defaultdict(int)
@@ -36,6 +37,14 @@ def isValid(s):
         unique_frequencies.add(curr_frequency)
         frequency_counts[curr_frequency] += 1
     
+    max_freq = max(unique_frequencies)
+    min_freq = min(unique_frequencies)
+    spread = abs(min_freq - max_freq)
+    
+    print("max, min freq", max_freq, min_freq)
+    print("unique frequencies", unique_frequencies)
+    print("frequency counts", frequency_counts)
+    
     # If there is only one unique freq, all letters occur same # of times
     if len(unique_frequencies) == 1:
         return 'YES'
@@ -43,18 +52,14 @@ def isValid(s):
     elif len(unique_frequencies) > 2:
         return 'NO'
     
-    # if max and min unique frequency are more than 2 degrees apart, cant be made valid with one deletion
-    max_freq = max(unique_frequencies)
-    min_freq = min(unique_frequencies)
-    if abs(min_freq - max_freq) > 1:
-        return 'NO'
-    # Otherwise, it can be made valid with one deletion
-    if frequency_counts[max_freq] <= 1:
-        return 'YES'
-    else:
-        return 'NO'
-        
-
+    elif len(unique_frequencies) == 2:
+        if frequency_counts[max_freq] <= 1 and spread <= 1:
+            return 'YES'
+        elif frequency_counts[min_freq] <=1 and min_freq == 1:
+            return 'YES'
+        else:
+            return 'NO'
+    
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
