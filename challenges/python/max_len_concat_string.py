0311936228a34e0395446176f1a111159ff72522
Firas S <choogiesaur@users.noreply.github.com>
2022-03-07 17:40:28 -0500

    Update max_len_concat_string.py

--- challenges/python/max_len_concat_string.py
+++ challenges/python/max_len_concat_string.py
@@ -21,3 +21,29 @@ class Solution:
                     print(concatenated)
                     max_length = max(max_length, len(concatenated))
         return max_length
+    
+# Recursive soln...not that much faster
+class Solution:
+    global_result = 0
+    def maxLength(self, arr: List[str]) -> int:
+        # Array, starting index, starting string
+        self.maxUnique(arr, 0, "")
+        return self.global_result
+
+    def num_unique_chars(self, s: str) -> bool:
+        encountered = set()
+        for char in s:
+            if char in encountered:
+                return -1
+            encountered.add(char)
+        return len(s)
+
+    def maxUnique(self, arr, i, curr):
+        if i == len(arr) and self.num_unique_chars(curr) > self.global_result:
+            self.global_result = len(curr) # or result of num_unique_chars
+            return
+        if i == len(arr):
+            return
+
+        self.maxUnique(arr, i+1, curr)
+        self.maxUnique(arr, i+1, curr + arr[i])
