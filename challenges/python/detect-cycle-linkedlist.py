aa6d2f8f3409fc6bd25de4f1cbcd850a161a26e9
Firas S <choogiesaur@users.noreply.github.com>
2022-03-09 22:57:21 -0500

    Cleaner set-based solution

--- challenges/python/detect-cycle-linkedlist.py
+++ challenges/python/detect-cycle-linkedlist.py
@@ -13,17 +13,10 @@ A Node is defined as:
 def has_cycle(head):
     observed_values = set()
 
-    while head:
-        # If we've seen this value before, start cycle search mode
-        if head.data in observed_values:
-            temp = head
-            while temp:
-                # From duplicate value node traverse further, if any reference equals the branching point, there is a cycle
-                if(temp is head):
-                    return True
-                temp = temp.next
-        # Add current node to list of observed
-        observed_values.add(head.data)
-        head = head.next
-    
+    curr = head
+    while curr:
+        if curr in observed_values:
+            return True
+        observed_values.add(curr)
+        curr = curr.next
     return False
