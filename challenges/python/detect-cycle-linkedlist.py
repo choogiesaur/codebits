f99acffcbe67adcd7fc17ce346c995cee95e3ff6
Firas S <choogiesaur@users.noreply.github.com>
2022-03-09 23:03:12 -0500

    Tortoise and Hare solution (cleaned)

--- challenges/python/detect-cycle-linkedlist.py
+++ challenges/python/detect-cycle-linkedlist.py
@@ -11,29 +11,29 @@ A Node is defined as:
 
 # Could also put hard limit on nodes searched to x, if there is such a constraint x on size of linkedlist
 class Solution:
-				def has_cycle(head):
-								observed_values = set()
+    def has_cycle(head):
+        observed_values = set()
 
-								curr = head
-								while curr:
-												if curr in observed_values:
-																return True
-												observed_values.add(curr)
-												curr = curr.next
-								return False
+        curr = head
+        while curr:
+            if curr in observed_values:
+                            return True
+            observed_values.add(curr)
+            curr = curr.next
+        return False
 
-				def hasCycle_alt(self, head: Optional[ListNode]) -> bool:
+    def hasCycle_alt(self, head: Optional[ListNode]) -> bool:
 
-								if not head:
-												return False
+        if not head:
+            return False
 
-								tortoise = head
-								hare = head.next
+        tortoise = head
+        hare = head.next
 
-								while tortoise and hare and hare.next:
-												if tortoise == hare:
-																return True
-												tortoise = tortoise.next
-												hare = hare.next.next
+        while tortoise and hare and hare.next:
+            if tortoise == hare:
+                            return True
+            tortoise = tortoise.next
+            hare = hare.next.next
 
-				return False
+        return False
