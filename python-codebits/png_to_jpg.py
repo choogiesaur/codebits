59f004ec123a594bebb1b15be95c0db062af708b
Firas S <choogiesaur@users.noreply.github.com>
2022-06-12 11:15:50 -0400

    Update png_to_jpg.py

--- python-codebits/png_to_jpg.py
+++ python-codebits/png_to_jpg.py
@@ -3,7 +3,7 @@ import glob
 
 # globs through directory for .png files, converts them using PIL (or Pillow) to .jpg
 for file in glob.glob("*.png"):
-	print file
+	print(file)
 	im = Image.open(file)
 	rgb_im = im.convert('RGB')
 	rgb_im.save(file.replace("png", "jpg"), quality=95)
