61783924566d9b53585849804ecb09dff903b9ae
Firas S <choogiesaur@users.noreply.github.com>
2023-02-03 11:33:00 -0500

    Add clean working solution

--- challenges/python/is-subsequence.py
+++ challenges/python/is-subsequence.py
@@ -1,35 +1,24 @@
 # LC 392
-# Passes most test cases
-# Fails this s = abc, t = ahbgdc
-# output: 
-# match 0 0
-# match 1 2
-# match 2 5
+
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        s_ptr = 0
-        t_ptr = 0
-
-        if s == t:
-            return True
-        elif len(s) > len(t):
-            return False
-
-        found_on_current = False
-        while s_ptr < len(s) and t_ptr < len(t):
-
-            while t_ptr < len(t):
-                if t[t_ptr] == s[s_ptr]:
-                    print("match", s_ptr, t_ptr)
-                    found_on_current = True
-                    t_ptr += 1
-                    break
-                t_ptr += 1
-
-            if t_ptr == len(t) and (found_on_current == False or s_ptr < len(s)):
-                return False
-
-            found_on_current = False
-            s_ptr += 1
         
-        return True
+        i = 0
+        j = 0
+
+        if len(s) > len(t):
+            return False
+        if len(s) == 0:
+            return True
+
+        while i < len(s):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+            if i == len(s):
+                return True
+            elif j == len(t) and i < len(s):
+                return False
