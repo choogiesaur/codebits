e71d385dafddc2bb7b8c1b138412ea8f0f1c08c4
Firas S <choogiesaur@users.noreply.github.com>
2023-07-14 17:21:39 -0400

    Update reverse-words.py
    Added a new, more Pythonic solution

--- challenges/python/reverse-words.py
+++ challenges/python/reverse-words.py
@@ -1,3 +1,4 @@
+# LC 151
 # Reverse words, removing trailing whitespace
 # " the sky is        blue       " ->
 # "blue is sky the"
@@ -5,6 +6,15 @@
 # words are letters+digits, string can contain spaces
 
 class Solution:
+    # Practice this one again, because I kept forgetting function usages
+    # Key points: python .isalnum(), reversed() takes in list as arg in parentheses, strip() to remove whitespace
+    def reverseWordsPythonic(self, s: str) -> str:
+        parts = s.split(" ")
+
+        ordered = [word.strip() for word in reversed(parts) if word.strip().isalnum() ]
+
+        return ' '.join(ordered)
+        
     def reverseWords(self, s: str) -> str:
         s = s.strip()
         reversed_tokens = []
