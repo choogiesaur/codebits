9ac06887e710c87c33aa6eaac6ed22f048ecfabb
Firas S <choogiesaur@users.noreply.github.com>
2023-07-27 17:09:23 -0400

    Create nth-tribonacci.py

---
+++ challenges/python/nth-tribonacci.py
@@ -0,0 +1,10 @@
+# LC 1137
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        d = {0: 0, 1: 1, 2: 1}
+
+        for i in range(3, n+1):
+            d[i] = d[i-1] + d[i-2] + d[i-3]
+            print(d[i])
+        
+        return d[n]
