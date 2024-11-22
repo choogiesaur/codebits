e7e254507a31849fcce1bc5513d3ed577ba84f78
Firas S. <choogiesaur@users.noreply.github.com>
2024-11-22 15:30:20 -0500

    between operator examp

--- sql/practice-queries.sql
+++ sql/practice-queries.sql
@@ -2,10 +2,22 @@
 
 SELECT first_name
 from patients
-where SUBSTRING(first_name, 1, 1) == 'C'
+where SUBSTRING(first_name, 1, 1) == 'C';
 
 -- OR, with regex:
 
 SELECT first_name
 from patients
-where first_name LIKE 'C%'
+where first_name LIKE 'C%';
+
+-- Weight in a range 100-120 inclusive:
+
+SELECT first_name, last_name
+from patients
+where weight >= 100 and weight <= 120;
+
+-- OR, with BETWEEN operator
+
+SELECT first_name, last_name
+FROM patients
+WHERE weight BETWEEN 100 AND 120;
