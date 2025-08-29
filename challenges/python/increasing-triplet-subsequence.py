d17641a359948033dfdb716be251245f1666f53f
Firas S. <choogiesaur@users.noreply.github.com>
2025-08-29 16:18:34 -0400

    comment

--- challenges/python/increasing-triplet-subsequence.py
+++ challenges/python/increasing-triplet-subsequence.py
@@ -1,6 +1,9 @@
 # lc334
 # in list, find indices i,j,k such that
 # a[i] < a[j] < a[k]
+
+# Dont need to store triplet or indices, 
+# Just see if we have seen a pattern of number, larger number, larger number
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         a = b = c = (2**31)-1
