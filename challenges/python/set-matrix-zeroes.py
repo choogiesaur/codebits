72d4c02c6024a568fb754df328c635c3b8d386ea
Firas S. <choogiesaur@users.noreply.github.com>
2025-09-26 16:17:37 -0400

    second solution

--- challenges/python/set-matrix-zeroes.py
+++ challenges/python/set-matrix-zeroes.py
@@ -27,3 +27,35 @@ class Solution:
         for j in range(len(matrix[0])):
             for row in offending_rows:
                 matrix[row][j] = 0
+
+
+    #2nd solution 2025
+    #previous one was better ;)
+    def setZeroes2(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix) # rows
+        n = len(matrix[0]) # cols
+        cleanup_list = []
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    cleanup_list.append((i,j))
+            
+        for item in cleanup_list:
+            x = item[0]
+            y = item[1]
+            
+            # lock y and 0 everything in this col
+            for i in range(m):
+                matrix[i][y] = 0
+                
+            # lock x and 0 everything in this row
+            for i in range(n):
+                matrix[x][i] = 0
+                
+            # IMPROVEMENT:
+            # if something is in the cleanup list, dont add any more pairs in its row or column
+            # as that row and column is already gonna get processed
