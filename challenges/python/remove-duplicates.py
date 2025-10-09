4d9d53e6cf5d0ffbe107fd89046b7393a049b4b7
Firas S. <choogiesaur@users.noreply.github.com>
2025-10-08 22:14:04 -0400

    Update remove-duplicates.py

--- challenges/python/remove-duplicates.py
+++ challenges/python/remove-duplicates.py
@@ -1,5 +1,23 @@
 # remove dupes in place
+# latest one....use two pointer tech!
+# slow keeps track of the next unique element in the final sorted order
+# fast explores the array to find the next unique element to swap into the slow pointers spot
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
 
+        if len(nums) == 1:
+            return 1
+
+        slow = 0
+        fast = 1
+        while fast < len(nums):
+            # nums are differnt, meaning we found a newly seen val
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+
+        return slow + 1
 # note on while/index:
 # reevaluates len(nums) on each iteration 
 # therefore deleting is safe as len will adjust
