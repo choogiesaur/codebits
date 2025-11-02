942d1dd85a880c4f7bb0094918a9f6c1e223ac20
Firas S. <choogiesaur@users.noreply.github.com>
2025-11-02 15:23:28 -0500

    Update merge-strings-alternately.py

--- challenges/python/merge-strings-alternately.py
+++ challenges/python/merge-strings-alternately.py
@@ -16,3 +16,16 @@ class Solution:
                 j += 1
         
         return out
+        
+# should be faster for not reconstructing string repeatedly 
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        (shorter, longer) = (word1, word2) if len(word1) < len(word2) else (word2, word1)
+        out = []
+        i = 0
+        print(longer, shorter)
+        while i < len(shorter):
+            out.append(word1[i])
+            out.append(word2[i])
+            i += 1
+        return ''.join(out) + str(longer[i:])
