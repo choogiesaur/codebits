d297ce726a6d9845ebaaea0b92187a210580bc4f
Firas S. <choogiesaur@users.noreply.github.com>
2025-11-18 18:03:58 -0500

    pythonic solution

--- challenges/python/reverse-words.py
+++ challenges/python/reverse-words.py
@@ -6,35 +6,7 @@
 # words are letters+digits, string can contain spaces
 
 class Solution:
-    # Practice this one again, because I kept forgetting function usages
-    # Key points: python .isalnum(), reversed() takes in list as arg in parentheses, strip() to remove whitespace
-    def reverseWordsPythonic(self, s: str) -> str:
-        parts = s.split(" ")
-
-        ordered = [word.strip() for word in reversed(parts) if word.strip().isalnum() ]
-
-        return ' '.join(ordered)
-        
     def reverseWords(self, s: str) -> str:
-        s = s.strip()
-        reversed_tokens = []
+        parts = [x for x in s.strip().split(' ') if len(x) > 0]
+        return ' '.join(parts[::-1])
         
-        curr_word = ''
-        i = 0
-        while i < len(s):
-            # start forming current word
-            if s[i] != ' ':
-                curr_word += s[i]
-            # if space encountered, record word in list and reset to empty string, then advance past all spaces
-            elif s[i] == ' ':
-                reversed_tokens = [ curr_word ] + reversed_tokens
-                curr_word = ''
-                while s[i] == ' ':
-                    i += 1
-                continue
-            i += 1
-        
-        if curr_word != '':
-            reversed_tokens = [ curr_word ] + reversed_tokens
-        
-        return ' '.join(reversed_tokens)
