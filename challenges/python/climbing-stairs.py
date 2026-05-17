330ae903ea26e4810736219a213df1d0dcbda465
Firas S. <choogiesaur@users.noreply.github.com>
2026-05-17 14:21:12 -0400

    Update climbing-stairs.py

--- challenges/python/climbing-stairs.py
+++ challenges/python/climbing-stairs.py
@@ -51,3 +51,23 @@ class Solution:
             d[i] = d[i-1] + d[i-2]
 
         return d[n]
+        
+    # 2026 try
+    # old ones are better :)    
+    class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def recurse(n):
+            if n == 0:
+                return 0
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+            if n in memo:
+                return memo[n]
+            memo[n] = recurse(n-1) + recurse(n-2)
+            return memo[n]
+        
+        
+        return recurse(n)
